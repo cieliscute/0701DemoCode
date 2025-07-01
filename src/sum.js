52a02f7edb09e328f13f07f476ab8e1d4564f27f
kaiwei <cielchang0324@gmail.com>
2025-07-01 23:43:01 +0800

    feat: 🎸 調整sum function
    改為輸出字串

--- src/sum.js
+++ src/sum.js
@@ -2,7 +2,8 @@ const { add } = require("./utils");
 
 function sum(a, b) {
   const result = add(a, b);
-  return result; // 這邊應該會是數字型別
+  // return result; // 這邊應該會是數字型別
+  return `result is ${result}`;
 }
 
 module.exports = { sum };
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,9 +1,9 @@
 const { sum } = require("./sum");
 
 test("sum(1, 2) should return 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe("result is 3");
 });
 
 test("sum(0.1, 0.2) should return 0.3", () => {
-  expect(sum(0.1, 0.2)).toBe(0.3);
+  expect(sum(0.1, 0.2)).toBe("result is 0.3");
 });
